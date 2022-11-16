5022aff9d9025a637f534f6383c4490b094ea55b
Georgiy Savelyev <savelevgeo.post@gmail.com>
2022-11-16 16:44:00 +0300

    download gdal_ttranslated raster

--- selfgeoref/main.js
+++ selfgeoref/main.js
@@ -5,7 +5,6 @@ import slfgrGeoRaster from './slfgr/GeoRaster';
 import slfgrGeoRef from './slfgr/GeoRef';
 
 const extentBtn = document.querySelector('.extent-btn');
-
 const map = new slfgrMap();
 
 const img = await slfgrRaster.fromPath('./data/Diamante_1_map_area.jpg');
@@ -34,5 +33,20 @@ console.log(img_gdal);
 const gcps = (await (await fetch('./data/gcps.txt')).text()).split(' ');
 const options = gcps.concat(['-of', 'GTiff', '-a_srs', 'EPSG:3857']);
 
-const translateResult = await geoRef.Gdal.gdal_translate(img_gdal, options);
-console.log(translateResult);
+geoRef.Gdal.gdal_translate(img_gdal, options)
+    .then( async () => {
+        const files = await geoRef.Gdal.getOutputFiles();
+        const filePath = files[0].path;
+        const fileBytes = await geoRef.Gdal.getFileBytes(filePath);
+        const fileName = filePath.split('/').pop();
+        saveAs(fileBytes, fileName);
+
+        function saveAs(fileBytes, fileName) {
+            const blob = new Blob([fileBytes]);
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = fileName;
+            link.click();
+        }
+    }
+);
