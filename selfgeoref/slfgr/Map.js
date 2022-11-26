77c93d2a97c8f374ac3403a6fb4d053f4685e596
Georgiy Savelyev <savelevspbu@mail.ru>
2022-11-26 17:50:37 +0300

    add elementary gcp table with no data

--- selfgeoref/slfgr/Map.js
+++ selfgeoref/slfgr/Map.js
@@ -6,31 +6,19 @@ import {OSM} from 'ol/source';
 import 'ol-layerswitcher/dist/ol-layerswitcher.css';
 import LayerSwitcher from 'ol-layerswitcher';
 
-class slfgrMap extends Map {
-    gcpSource = new VectorSource();
-    gcpStyle = {
-        'circle-stroke-color': '#1E90FF',
-        'circle-stroke-width': 1.25,
-        'circle-radius': 5,
-        'circle-fill-color': 'transparent'
-    };
-    gcpDrawStyle = {
-        'circle-stroke-color': '#484848',
-        'circle-stroke-width': 1.25,
-        'circle-radius': 5,
-        'circle-fill-color': 'transparent'
-    };
-    gcpSelectStyle = new Style({
-        image: new Circle({
-            fill: new Fill({ color: 'rgb(30, 144, 255)' }),
-            stroke: new Stroke({ color: 'white', width: 1.25 }),
-            radius: 5,
-        })
-    })
+import 'tabulator-tables/dist/css/tabulator.min.css';
+import {TabulatorFull as Tabulator} from 'tabulator-tables';
 
 import addGcpActions from './GCPActions';
 
 class slfgrMap extends Map {
+    gcpTable = new Tabulator('#gcp-table', {
+        layout: 'fitDataTable',
+        columns: [
+            {title: 'mapX', field: 'mapX'}
+        ]
+    });
+
     constructor() {
         super({
             target: 'map',
@@ -39,7 +27,6 @@ class slfgrMap extends Map {
         });
         
         this.addControl(new LayerSwitcher());
-        
         addGcpActions(this);
     }
 }
