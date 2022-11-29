b35d8b68bd5909eb0a02fb898db9c3a8f2638c92
Georgiy Savelyev <savelevgeo.post@gmail.com>
2022-11-29 20:35:23 +0300

    remove fit to extent button

--- selfgeoref/main.js
+++ selfgeoref/main.js
@@ -7,9 +7,6 @@ import Control from 'ol/control/Control';
 
 const map = new slfgrMap();
 
-const extentBtn = document.querySelector('.extent-btn');
-map.addControl(new Control({element: extentBtn}));
-
 //georef raster from upload
 const uploadBtn = document.querySelector('.upload-btn > input');
 const geoRef = await slfgrGeoRef.init();
@@ -28,32 +25,16 @@ uploadBtn.addEventListener('change', function() {
             console.log( Date.now() - startTime , 'image loaded');
 
             const raster = new slfgrRaster(img);
-            let rasterLayer;
-            extentBtn.disabled = false;
 
-            extentBtn.addEventListener(
-                'click',
-                () => addGcpActions(map),
-                {once: true}    
-            );
-            
-            extentBtn.addEventListener('click', () => {
-                map.removeLayer(rasterLayer);
-
-                rasterLayer = raster.fitToExtent(
-                    map.getView().calculateExtent()
-                );
-                const layers = map.getLayers();
-                layers.insertAt(layers.getLength() - 1, rasterLayer);
-            });
+            addGcpActions(map);
+            map.addLayer(raster.fitToExtent(map.getView().calculateExtent()));
 
             console.log( Date.now() - startTime , 'georef started');
             const gcps = (await (await fetch('./data/gcps.txt'))
                 .text()).split(' ');
 
             const geoRaster = await geoRef.byTable(raster, gcps);
-            const layers = map.getLayers();
-            layers.insertAt(layers.getLength() - 1, geoRaster.layer);
+            map.addLayer(geoRaster.layer);
             map.addControl(new Control({element: geoRaster.link}));
 
             console.log( Date.now() - startTime , 'georef finshed');
