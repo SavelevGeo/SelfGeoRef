254292dce7a348682a945eff607720d5a49d51c7
Georgiy Savelyev <savelevgeo.post@gmail.com>
2022-11-30 21:00:55 +0300

    separated image uploading and georeferencing by click

--- selfgeoref/main.js
+++ selfgeoref/main.js
@@ -9,9 +9,12 @@ const map = new slfgrMap();
 
 //georef raster from upload
 const uploadBtn = document.querySelector('.upload-btn > input');
+const georefBtn = document.querySelector('.georef-btn');
 const geoRef = await slfgrGeoRef.init();
+
 uploadBtn.addEventListener('change', function() {
-    console.log('image loading...');
+    console.time('image')
+    console.timeLog('image loading...');
 
     const img = new Image();
     img.file = this.files[0];
@@ -21,25 +24,27 @@ uploadBtn.addEventListener('change', function() {
     reader.onload = (e) => {
         img.src = e.target.result;
         img.onload = async () => {
-            console.time('image')
-            console.timeLog('image', 'image loaded');
+            console.timeEnd('image', 'image loaded');
 
             const raster = new slfgrRaster(img);
 
             addGcpActions(map);
             map.addLayer(raster.fitToExtent(map.getView().calculateExtent()));
 
-            document.querySelector('.georef-btn').disabled = false;
+            georefBtn.disabled = false;
+            georefBtn.addEventListener('click', async () => {
+                console.time('georef')
+                console.timeLog('georef', 'georef started');
+                const gcps = (await (await fetch('./data/gcps.txt'))
+                    .text()).split(' ');
+            
+                const geoRaster = await geoRef.byTable(raster, gcps);
+                map.addLayer(geoRaster.layer);
+                map.addControl(new Control({element: geoRaster.link}));
+            
+                console.timeEnd('georef', 'georef finished');
+            });
         };
     };
 });
 
-// console.timeLog('image', 'georef started');
-// const gcps = (await (await fetch('./data/gcps.txt'))
-//     .text()).split(' ');
-
-// const geoRaster = await geoRef.byTable(raster, gcps);
-// map.addLayer(geoRaster.layer);
-// map.addControl(new Control({element: geoRaster.link}));
-
-// console.timeLog('image', 'georef finished');
